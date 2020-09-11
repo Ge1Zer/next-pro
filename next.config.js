f3c5849bfb8d8d6c90f2015585fc32d8e8fc83bb
Superior <geizer@ro.ru>
2020-09-11 18:07:22 +0400

    'gg'

--- next.config.js
+++ next.config.js
@@ -2,14 +2,24 @@ const withVideos = require('next-videos')
 const withImages = require('next-images')
 
 
-module.exports =
-    withVideos(
-        withImages(
-            {assetDirectory: 'static',
-                webpack(config, options) {return config}
-            }
-        )
-    )
+module.exports = withVideos(withImages({
+    assetDirectory: 'static'
+    , webpack(config, options) {return config}
+    ,exportPathMap() {
+        return {
+            '/': {page: '/'},
+            '/i': {page: '/i'},
+            '/work': {page: '/work'},
+            '/library': {page: '/library'},
+            '/library/libraryPage': {page: '/libraryPage',query:{ side:"HTML5",page:'Simple' }},
+            '/library/libraryPage': {page: '/libraryPage',query:{ side:"CSS3",page:'Simple' }},
+            '/library/libraryPage': {page: '/libraryPage',query:{ side:"SASS",page:'Simple' }},
+            '/library/libraryPage': {page: '/libraryPage',query:{ side:"React",page:'Native' }},
+            '/library/libraryPage': {page: '/libraryPage',query:{ side:"Native",page:'Simple' }},
+        }
+    }
+    })
+)
 
 
 
