38b903497de9dcb6e27c8dd46bf90799a63d22b5
Superior <geizer@ro.ru>
2020-09-11 18:12:25 +0400

    'gg'

--- url.js
+++ url.js
@@ -1,8 +1,5 @@
 module.exports = {
-<<<<<<< HEAD
-     baseURI:'https://next-pro-mu.vercel.app'
-=======
-    baseURI:'https://next-pro-mu.vercel.app' 
->>>>>>> 0ffae189bfd9b30c1f4822fd2ea2f9287e7930f4
+    baseURI:'https://next-pro-mu.vercel.app'
+
     // baseURI:'http://localhost:3000'
 }
