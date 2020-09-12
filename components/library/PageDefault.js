dd7a3ed795e2ebe3a88e944fd01c69c0c7c77a41
Superior <geizer@ro.ru>
2020-09-12 12:02:47 +0400

    'ddd'

--- components/library/PageDefault.js
+++ components/library/PageDefault.js
@@ -1,9 +1,32 @@
+import styled from 'styled-components'
+import React from "react";
 import Layout from "../Layout";
 
+
+let LibraryPageDefault_style = styled.div`
+  font-size:3em;
+  height:100%;
+  display: flex;
+ 
+  justify-content: center;
+  align-items: center;
+  align-content: center;
+  &>p{
+    text-align: center ;
+    &>h2{
+        color:rgb(182,10,255);
+    }
+  }
+
+`
+
 export default function PageDefault(){
+
     return(
         <Layout>
-            page null
+            <LibraryPageDefault_style>
+                <p>The library is temporarily <h2>closed</h2></p>
+            </LibraryPageDefault_style>
         </Layout>
     )
 }
