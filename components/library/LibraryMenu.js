fbf8cac0ef7ab012422b8c045782c389c4881bc7
Superior <geizer@ro.ru>
2020-09-12 14:56:36 +0400

    'ddd'

--- components/library/LibraryMenu.js
+++ components/library/LibraryMenu.js
@@ -1,5 +1,5 @@
 import Link from "next/link";
-import React,{useState,useEffect} from "react";
+import React,{useState,useEffect,useLayoutEffect} from "react";
 import styled from 'styled-components'
 import {inject, observer} from "mobx-react";
 
@@ -38,52 +38,64 @@ const ListMenu = styled.ul`
     }
    
 `
-let MenuLibrary;
-export default MenuLibrary=inject('store')(observer(({store})=>{
 
-        useEffect(()=>{
-            store.menu.getListMenu()
-        },[])
+@inject('store')
+@observer
+export default class MenuLibrary extends React.Component{
+
+  constructor(props) {
+      super(props);
+
+      this.getRandomInt=this.getRandomInt.bind(this)
+  }
+  componentDidMount() {
+      this.props.store.menu.getListMenu()
+  }
 
 
-
-    function getRandomInt(max) {
+    getRandomInt(max) {
         return Math.floor(Math.random() * Math.floor(max));
     }
+
+    render()
+{
+
+
     return (
 
         <ListMenu>
-            {store.menu.menu.map(i => {
+            {this.props.store.menu.menu.map(i => {
 
-                let ri = getRandomInt(10000)
+                let ri = this.getRandomInt(10000)
                 return (
                     <li className={'list_1'}>
-                        <label htmlFor={`s${ri}`}  ><span>{i.name}</span></label>
+                        <label htmlFor={`s${ri}`}><span>{i.name}</span></label>
                         <input type={'checkbox'} id={`s${ri}`} checked/>
                         <ul>
-                            {i.list.sort(function(a, b){
-                                let nameA=a.frame.toLowerCase(), nameB=b.frame.toLowerCase()
+                            {i.list.sort(function (a, b) {
+                                let nameA = a.frame.toLowerCase(), nameB = b.frame.toLowerCase()
                                 if (nameA < nameB) //сортируем строки по возрастанию
                                     return -1
                                 if (nameA > nameB)
                                     return 1
                                 return 0 // Никакой сортировки
                             }).map(r => {
-                                let rie = getRandomInt(10000)
+                                let rie = this.getRandomInt(10000)
                                 return (
                                     <li className={'list_2'}>
-                                        <label htmlFor={`s${rie}`} ><span>{r.frame}</span></label>
+                                        <label htmlFor={`s${rie}`}><span>{r.frame}</span></label>
                                         <input type="checkbox" id={`s${rie}`}/>
                                         <ul>
-                                            {r.list.sort().map(e =>
+                                            {r.list.map(e =>
                                                 <li className={'list_3'}>
-                                                    <Link href={`/library/[id]`} as={`/library/${e.key}`}><a>{e.link}</a></Link>
+                                                    <Link href={`/library/[id]`}
+                                                          as={`/library/${e.key}`}><a>{e.link}</a></Link>
                                                 </li>
                                             )}
                                         </ul>
                                     </li>
                                 )
-                                })}
+                            })}
                         </ul>
                     </li>
                 )
@@ -91,4 +103,4 @@ export default MenuLibrary=inject('store')(observer(({store})=>{
             }
         </ListMenu>
     )
-}))
+}}
